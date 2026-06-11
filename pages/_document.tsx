4b0134a33d0e70ce2e98cc40a3570274739dc204
Hasan Baydoğan <hasan.b@aiport.tr>
2026-06-11 11:07:39 +0300

    sekme logosu değiştirildi

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="tr" className="dark">
       <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
